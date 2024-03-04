a7812460756b1f8e53e26f168465c369610b6be8
Sai Harsha <schapala@uncc.edu>
2024-03-03 23:30:04 -0500

    Updated Mongo

--- server.js
+++ server.js
@@ -1,36 +1,56 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const budgetModel = require("./schema");
+
 const app = express();
 const port = 3000;
-const data = require("./budget.json");
+const url = 'mongodb://localhost:27017/perbud';
 
 app.use('/', express.static('public'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-const budget = { 
-    myBudget: [
-    {
-        title: 'Eat Out',
-        budget: 30
-    },
-    {
-        title: 'Rent',
-        budget: 350
-    },
-    {
-        title: 'Groceries',
-        budget: 90
-    }
-]};
-
-app.get('/hello',(req,res)=>{
-    res.send('Hello world !');
+app.get('/budget', (req, res) => {
+    mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(() => {
+            console.log("Connected to database");
+            budgetModel.find({})
+                .then((data) => {
+                    res.send(data);
+                    mongoose.connection.close();
+                })
+                .catch((connectionError) => {
+                    console.log(connectionError);
+                });
+        })
+        .catch((connectionError) => {
+            console.log(connectionError);
+        });
 });
 
-app.get('/budget',(req,res)=>{
-    res.send(data);
+app.post("/newpostBudget", (req, res) => {
+    mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(() => {
+            console.log("Connected to database to insert data");
+            let newData = new budgetModel(req.body);
+            budgetModel.insertMany(newData)
+                .then(() => {
+                    res.send("Data Inserted into database Successfully");
+                    mongoose.connection.close();
+                })
+                .catch((connectionError) => {
+                    console.log(connectionError);
+                    res.send(connectionError.message);
+                });
+        })
+        .catch((connectionError) => {
+            console.log(connectionError);
+            res.send(connectionError);
+        });
 });
 
-
-
-app.listen(port, () =>{
-    console.log('Example app listening at http://localhost:${port}');
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
 });
